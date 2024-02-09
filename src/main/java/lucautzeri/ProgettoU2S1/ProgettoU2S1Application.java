e18ceebc0b56461ee31786b4256dc8f2532739fb
Luca Utzeri <lutzeri@libero.it>
2024-02-09 10:03:35 +0100

    Initial Commit: App Setup

---
+++ src/main/java/lucautzeri/ProgettoU2S1/ProgettoU2S1Application.java
@@ -0,0 +1,13 @@
+package lucautzeri.ProgettoU2S1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoU2S1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoU2S1Application.class, args);
+	}
+
+}
---
+++ src/test/java/lucautzeri/ProgettoU2S1/ProgettoU2S1ApplicationTests.java
@@ -0,0 +1,13 @@
+package lucautzeri.ProgettoU2S1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoU2S1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
